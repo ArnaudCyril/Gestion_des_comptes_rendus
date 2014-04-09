49eb4213e85be62f95a60b5278d0630d3cccb308
ArnaudCyril <04arnaud@gmail.com>
2014-04-09 16:46:26 +0200

    Version 3.0
    Ajout de javascript sur la page Rapport Visite.

--- formRAPPORT_VISITE.php
+++ formRAPPORT_VISITE.php
@@ -90,7 +90,62 @@
   <html><head>
 	<title>formulaire RAPPORT_VISITE</title>
 	
-	
+	<script language="javascript">
+		function selectionne(pValeur, pSelection,  pObjet) {
+			//active l'objet pObjet du formulaire si la valeur sélectionnée (pSelection) est égale à la valeur attendue (pValeur)
+			if (pSelection==pValeur) 
+				{ formRAPPORT_VISITE.elements[pObjet].disabled=false; }
+			else { formRAPPORT_VISITE.elements[pObjet].disabled=true; }
+		}
+		function active(maCheck, pObjet) {
+			//active ou desactive l'objet pObjet du formulaire selon la valeur de la checkbox
+			formRAPPORT_VISITE.elements[pObjet].disabled=!maCheck.checked; 
+		}
+	</script>
+	 <script language="javascript">
+        function ajoutLigne( pNumero){//ajoute une ligne de produits/qté à la div "lignes"  
+		     var laDiv=document.getElementById("lignes");	//récupère l'objet DOM qui contient les données
+             var nouvParagraphe=	document.createElement("p");
+			 laDiv.appendChild(nouvParagraphe);
+			//masque le bouton en cours
+			document.getElementById("but"+pNumero).setAttribute("hidden","true");	
+			pNumero++;										//incrémente le numéro de ligne
+            
+			var titre = document.createElement("label") ;	//crée un label
+			nouvParagraphe.appendChild(titre) ;		;				//l'ajoute à la DIV
+			titre.setAttribute("class","titre") ;			//définit les propriétés
+			titre.innerHTML= "   Produit : ";
+			var liste = document.createElement("select");	//ajoute une liste pour proposer les produits
+			nouvParagraphe.appendChild(liste) ;
+			liste.setAttribute("name","PRA_ECH"+pNumero) ;
+			liste.setAttribute("class","zone");
+			//remplit la liste avec les valeurs de la première liste construite en PHP à partir de la base
+			
+			
+			liste.innerHTML=formRAPPORT_VISITE.elements["PRA_ECH1"].innerHTML;
+			
+			var qte = document.createElement("label") ;	//crée un label
+			nouvParagraphe.appendChild(qte) ;		;				//l'ajoute à la DIV
+			qte.innerHTML= "Qte offerte : ";
+			
+			var qte = document.createElement("input");
+			nouvParagraphe.appendChild(qte);
+			qte.setAttribute("name","PRA_QTE"+pNumero);
+			qte.setAttribute("size","2"); 
+			qte.setAttribute("class","zone");
+			qte.setAttribute("type","text");
+			var bouton = document.createElement("input");
+			nouvParagraphe.appendChild(bouton);
+			
+			//ajoute une gestion évenementielle en faisant évoluer le numéro de la ligne
+			bouton.setAttribute("onClick","ajoutLigne("+ pNumero +");");
+			bouton.setAttribute("type","button");
+			bouton.setAttribute("value","+");
+			bouton.setAttribute("class","zone");	
+			bouton.setAttribute("id","but"+ pNumero);	
+			
+        }
+    </script>
 </head>
   <div id="contenu">
       <h2>Bienvenue sur l'intranet GSB</h2>
